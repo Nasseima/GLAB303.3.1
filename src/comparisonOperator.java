ba44673573a91eee1db6d26de5bf03af201ceade
Nasseima <lindsaynasseima@gmail.com>
2024-07-01 10:57:27 -0400

    All done

--- src/comparisonOperator.java
+++ src/comparisonOperator.java
@@ -1,2 +1,9 @@
 public class comparisonOperator {
+    public static void main (String [] args){
+        String s1 = "PerScholas";
+        String s2 = "PerScholas";
+        String s3 = new String("Perscholas");
+        System.out.println(s1==s2);
+        System.out.println(s1==s3);
+    }
 }
