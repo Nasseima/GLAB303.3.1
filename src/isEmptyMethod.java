96ff6e89ab5ae7f96307ced569f75cb4f522202e
Nasseima <lindsaynasseima@gmail.com>
2024-07-01 09:32:03 -0400

    Learning string methods in Java

---
+++ src/isEmptyMethod.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class isEmptyMethod {
+    public static void main(String[] args) {
+
+        String s1 = "";
+        String s2 = "hello";
+        System.out.println(s1.isEmpty());
+        System.out.println(s2.isEmpty());
+    }
+}
---
+++ src/trimMethod.java
@@ -0,0 +1,8 @@
+public class trimMethod {
+    public static void main(String [] args){
+
+        String s1 = "  hello  ";
+        System.out.println(s1 + "how are you");
+        System.out.println(s1.trim()+" how are you");
+    }
+}
